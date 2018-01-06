9bac35940bf2b55adff8301bef77c79c425595c0
Elijah <elijahsharper1@gmail.com>
2018-01-07 02:00:02 +0300

    PHP socket function and pure javascript add-up

--- app/Chat.php
+++ app/Chat.php
@@ -9,10 +9,14 @@ use Ratchet\MessageComponentInterface;
 class Chat implements MessageComponentInterface
 {
   protected $clients;
-  
-  function onOpen(ConnectionInterface $message)
-  {
 
+  function onOpen(ConnectionInterface $connection)
+  {
+    // echo get_class($connection);
+    // echo $connection->resourceId;
+    $this->clients[$connection->resourceId] = $connection;
+
+    // var_dump(count($this->clients)); check online clients (for future) 
   }
 
   function OnMessage(ConnectionInterface $connection, $message)
@@ -20,12 +24,12 @@ class Chat implements MessageComponentInterface
 
   }
 
-  function onClose(ConnectionInterface $message)
+  function onClose(ConnectionInterface $connection)
   {
-
+    unset($this->clients[$connection->resourceId]);
   }
 
-  function onError(ConnectionInterface $message, Exception $e)
+  function onError(ConnectionInterface $connection, Exception $e)
   {
     $connection->close();
   }
